b3e0c5887335e5f5f872a6636fe41f3d93ae0b06
Tidal <Tidal@192.168.0.107>
2020-05-05 15:17:36 -0500

    Added Customer Class

---
+++ src/main/java/BusinessLayer/Customer.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessLayer;
+
+/**
+ *
+ * @author Tidal
+ */
+public class Customer {
+    private int customerID;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Customer(int customerID, String firstName, String lastName, String phoneNumber) {
+        this.customerID = customerID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" + "customerID=" + customerID + ", firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber=" + phoneNumber + '}';
+    }
+    
+    
+}
